747d6814cfcf309e13c9b227048e2a3683e752bd
Vijay Patidar <41512842+vijaypatidar@users.noreply.github.com>
2022-10-12 02:47:12 +0530

    Update GraphQlJavaApplication.java

--- src/main/java/com/example/graphqljava/GraphQlJavaApplication.java
+++ src/main/java/com/example/graphqljava/GraphQlJavaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GraphQlJavaApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(GraphQlJavaApplication.class, args);
     }
 
